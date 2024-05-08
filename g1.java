5e4311bf67dc0f1dab8d133fe8b0d92f543e40ef
soundaryakumar2006 <soundaryak0000@gmail.com>
2024-05-08 11:21:23 +0530

    Java file

---
+++ g1.java
@@ -0,0 +1,7 @@
+public class g1 {
+    public static void main(String args[]){
+        System.out.println("My Second jenkins project");
+        System.out.println("And this is My jenkins changes");
+    }
+    
+}
