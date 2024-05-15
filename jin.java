7edf76f2d946a3fd5d6b21e8c1ebcd86834b2e45
soundaryakumar2006 <soundaryak0000@gmail.com>
2024-05-15 20:34:14 +0530

    java....

--- jin.java
+++ jin.java
@@ -1,6 +1,7 @@
 public class jin {
     public static void main(String args[]){
         System.out.println("My first jenkins project");
+        System.out.println("Blah Blah Blah My project.....");
     }
     
 }
